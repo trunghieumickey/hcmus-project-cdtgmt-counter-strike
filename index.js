405ad9b7a964e297b04fbcfc5c87c3881f7a936d
trunghieumickey <trunghieumickey@outlook.com.vn>
2023-12-18 15:06:29 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// Import Three.js from CDN
+import * as THREE from 'https://unpkg.com/three/build/three.module.min.js';
+import { OrbitControls } from './script/OrbitControls.js';
+import { GLTFLoader } from './script/GLTFLoader.js';
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(50, 50, 0);
+camera.lookAt(scene.position);
+
+// Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+// Create skybox
+const skyboxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
+const skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x87ceeb, side: THREE.BackSide });
+const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+scene.add(skybox);
+
+//create abient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+// Create control
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//import model
+const gltfLoader = new GLTFLoader();
+var mapModel;
+
+gltfLoader.load('./model/dust2.glb', (gltf) => {
+  mapModel = gltf.scene;
+  scene.add(mapModel);
+
+  // Create a bounding box
+  const box = new THREE.Box3().setFromObject(mapModel);
+
+  // Calculate the offset between the bounding box's center and the scene's center
+  const offset = box.getCenter(new THREE.Vector3()).negate();
+
+  // Apply the offset to the model's position
+  mapModel.position.add(offset);
+
+  // Now the center of the bounding box should be at the scene's center
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
