a7221f146dfee461292afed1b5f221fed6b395bf
Lê Trần Trung Hiếu <trunghieumickey@outlook.com.vn>
2023-12-19 14:29:52 +0700

    update import map

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import Three.js from CDN
-import * as THREE from 'https://unpkg.com/three/build/three.module.min.js';
-import { OrbitControls } from './script/OrbitControls.js';
-import { GLTFLoader } from './script/GLTFLoader.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // Create a scene
 const scene = new THREE.Scene();
