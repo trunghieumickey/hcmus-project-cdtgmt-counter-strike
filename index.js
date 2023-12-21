e4c5a8f6e9e621b354183e415f7fab82318fe766
trunghieumickey <trunghieumickey@outlook.com.vn>
2023-12-21 16:24:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,26 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
+import { Octree } from 'three/addons/math/Octree.js';
+import { OctreeHelper } from 'three/addons/helpers/OctreeHelper.js';
+import { Capsule } from 'three/addons/math/Capsule.js';
 
-// Create a scene
+const STEPS_PER_FRAME = 5;
+const GRAVITY = 30;
+
+const worldOctree = new Octree();
+const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 
+//create overview camera (top down, flat)
+const overviewCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+overviewCamera.position.set(0, 50, 0);
+overviewCamera.rotation.x = - Math.PI / 2;
+
+
 // Create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(50, 50, 0);
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.rotation.order = 'YXZ';
 camera.lookAt(scene.position);
 
 // Create a renderer
@@ -21,9 +34,6 @@ document.body.appendChild(renderer.domElement);
 //create abient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
-
-// Create control
-const controls = new OrbitControls(camera, renderer.domElement);
 
 //import model
 const gltfLoader = new GLTFLoader();
@@ -42,11 +52,217 @@ gltfLoader.load('./model/dust2.glb', (gltf) => {
   // Apply the offset to the model's position
   mapModel.position.add(offset);
 
+  worldOctree.fromGraphNode(mapModel);
   // Now the center of the bounding box should be at the scene's center
+  animate();
 });
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+window.addEventListener('resize', onWindowResize);
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
 }
-animate();
+
+
+// ====================== Player ======================
+
+const playerCollider = new Capsule(new THREE.Vector3(0, 0.35, 0), new THREE.Vector3(0, 1, 0), 0.35);
+const playerVelocity = new THREE.Vector3();
+const playerDirection = new THREE.Vector3();
+
+let playerOnFloor = false;
+let mouseTime = 0;
+
+const keyStates = {};
+
+const vector1 = new THREE.Vector3();
+const vector2 = new THREE.Vector3();
+const vector3 = new THREE.Vector3();
+
+document.addEventListener('keydown', (event) => {
+
+  keyStates[event.code] = true;
+
+});
+
+document.addEventListener('keyup', (event) => {
+
+  keyStates[event.code] = false;
+
+});
+
+document.addEventListener('mousedown', () => {
+
+  document.body.requestPointerLock();
+
+  mouseTime = performance.now();
+
+});
+
+document.addEventListener('mouseup', () => {
+
+  if (document.pointerLockElement !== null) throwBall();
+
+});
+
+document.body.addEventListener('mousemove', (event) => {
+
+  if (document.pointerLockElement === document.body) {
+
+    camera.rotation.y -= event.movementX / 500;
+    camera.rotation.x -= event.movementY / 500;
+
+  }
+
+});
+
+//listen for tab key
+
+function playerCollisions() {
+
+  const result = worldOctree.capsuleIntersect(playerCollider);
+
+  playerOnFloor = false;
+
+  if (result) {
+
+    playerOnFloor = result.normal.y > 0;
+    
+
+    if (!playerOnFloor) {
+
+      playerVelocity.addScaledVector(result.normal, - result.normal.dot(playerVelocity));
+
+    }
+
+    playerCollider.translate(result.normal.multiplyScalar(result.depth));
+
+  }
+
+}
+
+function updatePlayer(deltaTime) {
+
+  let damping = Math.exp(- 4 * deltaTime) - 1;
+
+  if (!playerOnFloor) {
+
+    playerVelocity.y -= GRAVITY * deltaTime;
+
+    // small air resistance
+    damping *= 0.1;
+
+  }
+
+  playerVelocity.addScaledVector(playerVelocity, damping);
+
+  const deltaPosition = playerVelocity.clone().multiplyScalar(deltaTime);
+  playerCollider.translate(deltaPosition);
+
+  playerCollisions();
+
+  camera.position.copy(playerCollider.end);
+
+}
+
+function getForwardVector() {
+
+  camera.getWorldDirection(playerDirection);
+  playerDirection.y = 0;
+  playerDirection.normalize();
+
+  return playerDirection;
+
+}
+
+function getSideVector() {
+
+  camera.getWorldDirection(playerDirection);
+  playerDirection.y = 0;
+  playerDirection.normalize();
+  playerDirection.cross(camera.up);
+
+  return playerDirection;
+
+}
+
+function controls(deltaTime) {
+
+  // gives a bit of air control
+  const speedDelta = deltaTime * (playerOnFloor ? 25 : 8);
+
+  if (keyStates['KeyW']) {
+
+    playerVelocity.add(getForwardVector().multiplyScalar(speedDelta));
+
+  }
+
+  if (keyStates['KeyS']) {
+
+    playerVelocity.add(getForwardVector().multiplyScalar(- speedDelta));
+
+  }
+
+  if (keyStates['KeyA']) {
+
+    playerVelocity.add(getSideVector().multiplyScalar(- speedDelta));
+
+  }
+
+  if (keyStates['KeyD']) {
+
+    playerVelocity.add(getSideVector().multiplyScalar(speedDelta));
+
+  }
+
+  if (playerOnFloor) {
+
+    if (keyStates['Space']) {
+
+      playerVelocity.y = 8;
+
+    }
+
+  }
+
+}
+
+function teleportPlayerIfOob() {
+
+  if (camera.position.y <= - 25) {
+
+    playerCollider.start.set(0, 0.35, 0);
+    playerCollider.end.set(0, 1, 0);
+    playerCollider.radius = 0.35;
+    camera.position.copy(playerCollider.end);
+    camera.rotation.set(0, 0, 0);
+
+  }
+}
+
+function animate() {
+  console.log(worldOctree.objects);
+  const deltaTime = Math.min(0.05, clock.getDelta()) / STEPS_PER_FRAME;
+
+  // we look for collisions in substeps to mitigate the risk of
+  // an object traversing another too quickly for detection.
+
+  for (let i = 0; i < STEPS_PER_FRAME; i++) {
+
+    controls(deltaTime);
+    updatePlayer(deltaTime);
+    teleportPlayerIfOob();
+  }
+  if (keyStates['KeyQ']) {
+    renderer.render(scene, overviewCamera);
+  } else {
+  renderer.render(scene, camera);
+  }
+  requestAnimationFrame(animate);
+
+}
